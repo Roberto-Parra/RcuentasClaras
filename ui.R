214a49b4a5012e35eec6ad6ef9ea2cfea42a22fb
Roberto-Parra <w.parrav@uniandes.edu.co>
2021-02-27 21:49:09 -0300

    Creación de las etiqueta con textos de bienvenida

--- ui.R
+++ ui.R
@@ -1,6 +1,11 @@
-ui <- fluidPage(theme = bslib::bs_theme(),
-                titlePanel("Cuentas Claras"),
-                mainPanel()
+ui <- fluidPage(theme = bslib::bs_theme(version = 4, bootswatch = "minty"),
+                includeCSS("www/style.css"),
+                titlePanel(h1(id="title","Cuentas Claras", align = "center")),
                 
+                column(12,align="center",h4("!!!Bienvenido a CuentasClaras!!")),
+                column(12,align="center",h4("La unica aplicación que te permite organizar actividades con tus amigos de forma justa")),
+                mainPanel(
+                        
+                )
                 
                 )
