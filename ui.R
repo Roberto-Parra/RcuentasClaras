e1ce8b99f1515d1862a1459310cfca8ce071a87a
Roberto-Parra <w.parrav@uniandes.edu.co>
2021-02-27 22:19:18 -0300

    Se agregan botones

--- ui.R
+++ ui.R
@@ -1,11 +1,16 @@
-ui <- fluidPage(theme = bslib::bs_theme(version = 4, bootswatch = "minty"),
-                includeCSS("www/style.css"),
-                titlePanel(h1(id="title","Cuentas Claras", align = "center")),
-                
-                column(12,align="center",h4("!!!Bienvenido a CuentasClaras!!")),
-                column(12,align="center",h4("La unica aplicación que te permite organizar actividades con tus amigos de forma justa")),
-                mainPanel(
-                        
-                )
-                
-                )
+ui <- fluidPage(
+        theme = bslib::bs_theme(version = 4, bootswatch = "minty"),
+        includeCSS("www/style.css"),
+        titlePanel(h1(id="title","Cuentas Claras", align = "center")),
+        column(12,align="center",h4("!!!Bienvenido a CuentasClaras!!")),
+        column(12,align="center",h4("La unica aplicación que te permite organizar actividades con tus amigos de forma justa")),
+        column(12,br()),
+        column(12,
+               tags$div(
+                        class = "d-flex justify-content-center",
+                        column(4,align='left',actionButton('add','Añadir Actividad',width='70%')),
+                        column(4,br()),
+                        column(4,align='right',actionButton('viewv','Ver Viajeros',width='70%'))
+                )),
+        mainPanel()
+        )
